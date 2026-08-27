b0267eccf79f241f08be33fa3aed446a86f3e18c
JhoanStevenPlataforma <desarrollador.signage@plataforma.com.co>
2026-08-26 22:45:58 -0500

    feat: implementar endpoints de autenticacion, refresh, logout y ping

---
+++ src/main/java/com/sena/mesa/mesaayuda/controllers/PingController.java
@@ -0,0 +1,14 @@
+package com.sena.mesa.mesaayuda.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/api/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
